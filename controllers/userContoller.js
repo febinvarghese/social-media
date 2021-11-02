370ef17a4a6452b020333ff284b08ada746ca6e6
Febin Varghese <febin9922@gmail.com>
2021-11-02 18:59:36 +0530

    added some changes

--- controllers/userContoller.js
+++ controllers/userContoller.js
@@ -1,14 +1,32 @@
 const User = require("../models/User");
 
-
-
-
 exports.getAllUsers = async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      const { password, _id, ...other } = user._doc;
-      res.status(200).json(other);
-    } catch (error) {
-      res.status(500).json(error);
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, _id, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+// follow a user 
+
+ exports.followuser =    async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        const currentUser = await User.findById(req.body.userId);
+  
+        if (!user.followers.includes(req.body.userId)) {
+          await user.updateOne({ $push: { followers: req.body.userId } });
+          await currentUser.updateOne({ $push: { followin: req.params.id } });
+          res.status(202).json("you are now following this user");
+        } else {
+          res.status(403).json("you have already followed this user");
+        }
+      } catch (error) {}
+    } else {
+      res.status(200).json("you cannot follow yourself");
     }
-  }  ;
+  }
--- route/users.js
+++ route/users.js
@@ -59,24 +59,7 @@ router.delete("/:id", async (req, res) => {
 router.route('/:id').get(userContoller.getAllUsers)
 
 // follow a user
-router.put("/:id/follow", async (req, res) => {
-  if (req.body.userId !== req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId);
-
-      if (!user.followers.includes(req.body.userId)) {
-        await user.updateOne({ $push: { followers: req.body.userId } });
-        await currentUser.updateOne({ $push: { followin: req.params.id } });
-        res.status(202).json("you are now following this user");
-      } else {
-        res.status(403).json("you have already followed this user");
-      }
-    } catch (error) {}
-  } else {
-    res.status(200).json("you cannot follow yourself");
-  }
-});
+router.route('/:id/follow').put(userContoller.followuser)
 
 
 // unfollow a user 
